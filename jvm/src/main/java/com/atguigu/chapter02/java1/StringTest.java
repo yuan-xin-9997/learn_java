5cb94103bce6b98f9ed86377437d27ccb901e065
yuan.xin <yuanxin9997@qq.com>
2024-12-05 20:51:16 +0800

    jvm

---
+++ jvm/src/main/java/com/atguigu/chapter02/java1/StringTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.chapter02.java1;
+
+/**
+ * @author shkstart
+ * @create 2020 上午 11:39
+ */
+public class StringTest {
+
+    public static void main(String[] args) {
+        String str = new String();
+        System.out.println("hello,atguigu.com");
+
+        StringTest test = new StringTest();
+        System.out.println(test.getClass().getClassLoader());
+    }
+}
